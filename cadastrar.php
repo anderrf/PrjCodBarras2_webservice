3ae2a846be80e42e745534be4be1e3cc9d8faf87
Anderson <andersonrochafarias@gmail.com>
2019-06-11 19:48:42 +0000

    Cadastro

---
+++ cadastrar.php
@@ -0,0 +1,27 @@
+<?php
+    try{
+        
+        $conexao = mysqli_connect("localhost", "andersonrf", "", "bd_mobile");
+                                    //Servidor  usuário     senha   banco
+                                    
+        $nome = $_POST['nome'];
+        $cod = $_POST['cod'];
+        $valor = $_POST['valor'];
+        $descricao = $_POST['descricao'];
+        $processador = $_POST['processador'];
+        $sistemaop = $_POST['sistemaop'];
+        $tamtela = $_POST['tamtela'];
+        $tecwifi = $_POST['tecwifi'];
+        $qtcam = $_POST['qtcam'];
+        $rescam = $_POST['rescam'];
+        $memflash = $_POST['memflash'];
+        
+        $query = "INSERT INTO celular VALUES (NULL, '$nome', '$cod', '$valor', '$descricao', '$processador', '$sistemaop', '$tamtela', '$tecwifi', '$qtcam', '$rescam', '$memflash');";
+        mysqli_query($conexao, $query);
+        
+        echo "Cadastro realizado com sucesso.";
+        
+        
+    }catch (Exception $e) {
+        echo "Falha: " . $e;
+    }
